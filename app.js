f14e3a20cd4b3435144a6e06b7e91ba643da47df
yysaki <yysaki@yysaki.com>
2015-05-10 13:05:54 +0900

    app.js: Refine as es6 script

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
+import express from 'express';
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
+app.get('/', (request, response) => {
   response.render('index', { title: 'Hey', message: 'Hello there!' });
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), () => {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
