89d0597b3baecde04094d36dd12a16148227d729
yysaki <yysaki@yysaki.com>
2015-05-17 16:45:37 +0900

    app.js: Load shiren4_udewa

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import * as shiren4_udewa from './shiren4_udewa';
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -6,7 +8,7 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
-  response.render('index', { title: 'Hey', message: 'Hello there!' });
+  response.render('index', { data: shiren4_udewa });
 });
 
 app.listen(app.get('port'), () => {
