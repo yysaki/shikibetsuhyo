b3c26263812a2b72117c18acd3bedf50ac89d782
yysaki <yysaki@yysaki.com>
2015-05-17 16:38:49 +0900

    Add shiren4_udewa.js

---
+++ shiren4_udewa.js
@@ -0,0 +1,32 @@
+export default {
+  type: 'udewa',
+  name: '腕輪',
+  items: [
+    {name: '遠投の腕輪',       buy: 2000,  sell: 700},
+    {name: 'ちからの腕輪',     buy: 2000,  sell: 700},
+    {name: 'ヘタ投げの腕輪',   buy: 2000,  sell: 700},
+    {name: '混乱よけの腕輪',   buy: 3000,  sell: 1050},
+    {name: '睡眠よけの腕輪',   buy: 3000,  sell: 1050},
+    {name: '痛恨の腕輪',       buy: 3000,  sell: 1050},
+    {name: '毒消しの腕輪',     buy: 3000,  sell: 1050},
+    {name: '呪いよけの腕輪',   buy: 3000,  sell: 1050},
+    {name: '保持の腕輪',       buy: 3000,  sell: 1050},
+    {name: '魔物呼びの腕輪',   buy: 3000,  sell: 1050},
+    {name: 'ワナの腕輪',       buy: 3000,  sell: 1050},
+    {name: '裏道の腕輪',       buy: 5000,  sell: 1750},
+    {name: '回復の腕輪',       buy: 5000,  sell: 1750},
+    {name: '壁抜けの腕輪',     buy: 5000,  sell: 1750},
+    {name: '気配察知の腕輪',   buy: 5000,  sell: 1750},
+    {name: '高飛びの腕輪',     buy: 5000,  sell: 1750},
+    {name: '道具感知の腕輪',   buy: 5000,  sell: 1750},
+    {name: '爆発の腕輪',       buy: 5000,  sell: 1750},
+    {name: '水グモの腕輪',     buy: 5000,  sell: 1750},
+    {name: 'しあわせの腕輪',   buy: 10000, sell: 3500},
+    {name: '弾きよけの腕輪',   buy: 10000, sell: 3500},
+    {name: '鑑定師の腕輪',     buy: 30000, sell: 10500},
+    {name: '透視の腕輪',       buy: 30000, sell: 10500},
+    {name: 'ブルドッ君の腕輪', buy: 30000, sell: 10500},
+    {name: 'ワナ師の腕輪',     buy: 30000, sell: 10500}
+  ]
+}
+
