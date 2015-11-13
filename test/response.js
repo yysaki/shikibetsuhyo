4f5a8f5c5242f55d64e987553f85d7d51214759f
yysaki <yysaki@mail.yysaki.com>
2015-11-13 22:49:59 +0900

    test/response.js: Add acceptable test for "/" route

--- test/response.js
+++ test/response.js
@@ -8,7 +8,7 @@ describe('root path', () => {
       .get('/')
       .expect(200)
       .end((err, res) => {
-        expect(res.text).to.contain('<title>値段識別表 - 不思議のダンジョン 風来のシレン4 神の眼と悪魔のヘソ</title>');
+        expect(res.text).to.contain('<title>不思議のダンジョン 値段識別表</title>');
 
         if (err) return done(err);
         done();
