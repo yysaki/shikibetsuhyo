e996c1482ac3b33bc880eba35841f3878a7748cb
yysaki <yysaki@mail.yysaki.com>
2015-11-13 22:50:07 +0900

    test/response.js: Add acceptable test for "/shiren4/" route

--- test/response.js
+++ test/response.js
@@ -15,3 +15,17 @@ describe('root path', () => {
       });
   });
 });
+
+describe('shiren4 path', () => {
+  it('should return 200 with correct title', done => {
+    request(app)
+      .get('/shiren4/')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.text).to.contain('<title>風来のシレン4 神の眼と悪魔のヘソ - 不思議のダンジョン 値段識別表</title>');
+
+        if (err) return done(err);
+        done();
+      });
+  });
+});
