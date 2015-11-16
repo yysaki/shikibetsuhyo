e9bf9d988b42ea52264f6ff25f0a0df507cd256b
yysaki <yysaki@mail.yysaki.com>
2015-11-16 23:07:19 +0900

    app.js: Add new route to "/"

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/shiren/', (request, response) => {
+app.get('/', (request, response) => {
+  response.render('index');
+});
+
+app.get('/shiren4/', (request, response) => {
   var about = 'about/shiren4';
 
   response.render(about, (err, aboutHtml) => {
     response.render(
-      'index',
+      'content',
       {
         data: shiren4.data,
         about: aboutHtml
