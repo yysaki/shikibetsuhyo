5a964181c3dcab0a75c5041d6bb86d791bc9ebec
yysaki <yysaki@mail.yysaki.com>
2015-12-12 12:57:35 +0900

    Add test for ItemList.isHeadOfGroup

--- test-es6/game-test.js
+++ test-es6/game-test.js
@@ -7,3 +7,30 @@ describe('Game', () => {
     expect(true).to.is.true;
   });
 });
+
+describe('ItemList', () => {
+  it('should isHeadOfGroup correctly', () => {
+    var hudaList = new ItemList({
+      type: 'huda',
+      name: '札',
+      items: [
+        new Item({name: '睡眠の札',       buy: 500, sell: 175}),
+        new Item({name: '封印の札',       buy: 500, sell: 175}),
+        new Item({name: '混乱の札',       buy: 500, sell: 175}),
+        new Item({name: '空振りの札',     buy: 500, sell: 175}),
+        new Item({name: 'ゾワゾワの札',   buy: 500, sell: 175}),
+        new Item({name: '影縫いの札',     buy: 500, sell: 175}),
+        new Item({name: '加速の札',       buy: 500, sell: 175}),
+        new Item({name: '鈍足の札',       buy: 500, sell: 175}),
+        new Item({name: '狂戦士の札',     buy: 500, sell: 175}),
+        new Item({name: 'イカリの札',     buy: 500, sell: 175}),
+        new Item({name: 'オオイカリの札', buy: 500, sell: 175})
+      ]
+    });
+    var suimin = new Item({name: '睡眠の札', buy: 500, sell: 175});
+    var huuin  = new Item({name: '封印の札', buy: 500, sell: 175});
+
+    expect(hudaList.isHeadOfGroup(suimin)).to.be.true;
+    expect(hudaList.isHeadOfGroup(huuin)).to.be.false;
+  });
+});
