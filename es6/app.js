d9cf5a38c4326a441b98c5edec2958b35ab550bd
yysaki <yysaki@yysaki.com>
2018-08-11 22:53:01 +0900

    Refine app.js to care firebase cloud functions

--- es6/app.js
+++ es6/app.js
@@ -1,29 +1,21 @@
+const functions = require('firebase-functions');
 import express from 'express';
-import gameList from './model/game-list.js';
+const gameList = require('./model/game-list.js');
 
 var app = express();
-var basedir = __dirname
-  .replace(/\\/g, '/')
-  .replace(/\/[^\/]*\/?$/, '');
 
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'pug');
-app.use(express.static(basedir + '/public'));
+app.set('views', './views');
 
 app.get('/', (request, response) => {
   response.render('index');
 });
 
-gameList.forEach(g => {
+gameList.default.forEach(g => {
   app.get(`/${g.id}/`, (request, response) => {
     response.render('content', {game: g});
   });
 })
 
-module.exports = app;
-
-if (!module.parent) {
-  app.listen(app.get('port'), () => {
-    console.log("Node app is running at localhost:" + app.get('port'))
-  });
-}
+exports.app = functions.https.onRequest(app);
