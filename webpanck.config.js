047d022dcba57d3a1e4bd008e0eb53ea18014dec
yysaki <yysaki@mail.yysaki.com>
2018-09-21 21:45:25 +0900

    Install webpack

---
+++ webpanck.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './es6/app.js',
+  output: {
+    filename: 'bundle.min.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
