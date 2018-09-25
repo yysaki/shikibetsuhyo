8be34c4c4f82c8726345d2196f25a19702ca1bd5
yysaki <yysaki@mail.yysaki.com>
2018-09-25 21:48:10 +0900

    pages/index: Show game titles

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
 <template>
-  <p>Hello Vue!</p>
+  <div id="index">
+    <ul>
+      <li v-for="game in games">
+        {{ game.title }}
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
+const games = require('~/es6/model/game-list').default;
 export default {
-  name: "index"
+  name: "index",
+  data: function () {
+    return {
+      games: games
+    }
+  }
 };
 </script>
+
