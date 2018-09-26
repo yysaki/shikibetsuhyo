0d3bb3b90c9966eaa606af5abd6a80088eeab0e7
yysaki <yysaki@mail.yysaki.com>
2018-09-26 21:23:23 +0900

    Set vuetify theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   modules: [
     '@nuxtjs/vuetify'
-  ]
+  ],
+  vuetify: {
+    theme: {
+      primary: '#3e62ad',
+      secondary: '#7986CB',
+      accent: '#9c27b0',
+      error: '#f44336',
+      warning: '#ffeb3b',
+      info: '#2196f3',
+      success: '#4caf50'
+    }
+  }
 }
