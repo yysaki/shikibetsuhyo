4fe75884a75242cff4078316095bb5eda0a88c71
yysaki <yysaki@mail.yysaki.com>
2018-09-26 21:24:13 +0900

    Add layouts/dafault

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app id="layout">
+  <v-toolbar
+    color="primary"
+    dark
+  >
+    <v-toolbar-title>
+      不思議のダンジョン
+    </v-toolbar-title>
+  </v-toolbar>
+    <nuxt/>
+  </v-app>
+</template>
+<scripts>
+export default {
+  name: "layout"
+};
+</scripts>
