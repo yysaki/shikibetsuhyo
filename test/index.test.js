dcd62222655caeb7176cfbe9053aa84ac2bdab6a
yysaki <yysaki@mail.yysaki.com>
2018-10-04 20:11:32 +0900

    test/index.test.js: Add test for /shiren/ tab header

--- test/index.test.js
+++ test/index.test.js
@@ -52,6 +52,17 @@ test('Route /shiren5/ exits and render HTML', async t => {
   t.true(html.includes('風来のシレン5 フォーチュンタワーと運命のダイス - 不思議のダンジョン 値段識別表'));
 });
 
+test('Route /shiren/ exits and render tab header', async t => {
+  let context = {}
+  const { html } = await nuxt.renderRoute('/shiren/', context);
+
+  t.true(html.includes('腕輪',));
+  t.true(html.includes('草',));
+  t.true(html.includes('巻物',));
+  t.true(html.includes('壺',));
+  t.true(html.includes('杖',));
+});
+
 test.after('Closing server', t => {
   nuxt.close();
 })
