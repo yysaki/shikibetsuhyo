85c8ca7c501a29239aabdae8c85c875de20b2d42
yysaki <yysaki@mail.yysaki.com>
2018-10-04 20:12:22 +0900

    test/index.test.js: Update test description for root

--- test/index.test.js
+++ test/index.test.js
@@ -22,31 +22,31 @@ test('Route / exits and render HTML', async t => {
   t.true(html.includes('不思議のダンジョン 値段識別表'));
 });
 
-test('Route /shiren exits and render HTML', async t => {
+test('Route /shiren/ exits and render HTML', async t => {
   let context = {}
   const { html } = await nuxt.renderRoute('/shiren/', context);
   t.true(html.includes('不思議のダンジョン2 風来のシレン - 不思議のダンジョン 値段識別表'));
 });
 
-test('Route /shiren2 exits and render HTML', async t => {
+test('Route /shiren2/ exits and render HTML', async t => {
   let context = {}
   const { html } = await nuxt.renderRoute('/shiren2/', context);
   t.true(html.includes('風来のシレン2 ～鬼襲来!シレン城!～ - 不思議のダンジョン 値段識別表'));
 });
 
-test('Route /shiren3 exits and render HTML', async t => {
+test('Route /shiren3/ exits and render HTML', async t => {
   let context = {}
   const { html } = await nuxt.renderRoute('/shiren3/', context);
   t.true(html.includes('風来のシレン3 からくり屋敷の眠り姫 - 不思議のダンジョン 値段識別表'));
 });
 
-test('Route /shiren4 exits and render HTML', async t => {
+test('Route /shiren4/ exits and render HTML', async t => {
   let context = {}
   const { html } = await nuxt.renderRoute('/shiren4/', context);
   t.true(html.includes('風来のシレン4 神の眼と悪魔のヘソ - 不思議のダンジョン 値段識別表'));
 });
 
-test('Route /shiren5 exits and render HTML', async t => {
+test('Route /shiren5/ exits and render HTML', async t => {
   let context = {}
   const { html } = await nuxt.renderRoute('/shiren5/', context);
   t.true(html.includes('風来のシレン5 フォーチュンタワーと運命のダイス - 不思議のダンジョン 値段識別表'));
