92c81becfcaaf4c359904f29e745dd5d0aae5213
yysaki <yysaki@mail.yysaki.com>
2018-10-15 20:36:13 +0900

    game.test.js: Fix to treat as ava

--- test/game.test.js
+++ test/game.test.js
@@ -1,47 +1,42 @@
-import {Game, ItemList, Item} from '../js/model/game.js';
-import {expect} from 'chai';
+import test from 'ava';
+import { resolve } from 'path';
+import {Game, ItemList, Item} from '../es6/model/game.js';
 
-describe('Game', () => {
-  it('should be loaded', () => {
-    var g = new Game({title: "", shortTitle: "", lists: []});
-    expect(true).to.is.true;
-  });
+test('Game should be loaded', async t => {
+  var g = new Game({title: "", shortTitle: "", lists: []});
+  t.true(true);
 });
 
-describe('ItemList', () => {
-  it('should isHeadOfGroup correctly', () => {
-    var hudaList = new ItemList({
-      type: 'huda',
-      name: '札',
-      items: [
-        new Item({name: '睡眠の札',       buy: 500, sell: 175}),
-        new Item({name: '封印の札',       buy: 500, sell: 175}),
-        new Item({name: '混乱の札',       buy: 500, sell: 175}),
-        new Item({name: '空振りの札',     buy: 500, sell: 175}),
-        new Item({name: 'ゾワゾワの札',   buy: 500, sell: 175}),
-        new Item({name: '影縫いの札',     buy: 500, sell: 175}),
-        new Item({name: '加速の札',       buy: 500, sell: 175}),
-        new Item({name: '鈍足の札',       buy: 500, sell: 175}),
-        new Item({name: '狂戦士の札',     buy: 500, sell: 175}),
-        new Item({name: 'イカリの札',     buy: 500, sell: 175}),
-        new Item({name: 'オオイカリの札', buy: 500, sell: 175})
-      ]
-    });
-    var suimin = new Item({name: '睡眠の札', buy: 500, sell: 175});
-    var huuin  = new Item({name: '封印の札', buy: 500, sell: 175});
-
-    expect(hudaList.isHeadOfGroup(suimin)).to.be.true;
-    expect(hudaList.isHeadOfGroup(huuin)).to.be.false;
+test('ItemList should isHeadOfGroup correctly', async t => {
+  var hudaList = new ItemList({
+    type: 'huda',
+    name: '札',
+    items: [
+      new Item({name: '睡眠の札',       buy: 500, sell: 175}),
+      new Item({name: '封印の札',       buy: 500, sell: 175}),
+      new Item({name: '混乱の札',       buy: 500, sell: 175}),
+      new Item({name: '空振りの札',     buy: 500, sell: 175}),
+      new Item({name: 'ゾワゾワの札',   buy: 500, sell: 175}),
+      new Item({name: '影縫いの札',     buy: 500, sell: 175}),
+      new Item({name: '加速の札',       buy: 500, sell: 175}),
+      new Item({name: '鈍足の札',       buy: 500, sell: 175}),
+      new Item({name: '狂戦士の札',     buy: 500, sell: 175}),
+      new Item({name: 'イカリの札',     buy: 500, sell: 175}),
+      new Item({name: 'オオイカリの札', buy: 500, sell: 175})
+    ]
   });
+  var suimin = new Item({name: '睡眠の札', buy: 500, sell: 175});
+  var huuin  = new Item({name: '封印の札', buy: 500, sell: 175});
+
+  t.true(hudaList.isHeadOfGroup(suimin));
+  t.false(hudaList.isHeadOfGroup(huuin));
 });
 
-describe('Item', () => {
-  it('should equals correctly', () => {
-    var suimin = new Item({name: '睡眠の札', buy: 500, sell: 175});
-    var suimin2 = new Item({name: '睡眠の札', buy: 500, sell: 175});
-    var huuin  = new Item({name: '封印の札', buy: 500, sell: 175});
+test('Item should equals correctly', async t => {
+  var suimin = new Item({name: '睡眠の札', buy: 500, sell: 175});
+  var suimin2 = new Item({name: '睡眠の札', buy: 500, sell: 175});
+  var huuin  = new Item({name: '封印の札', buy: 500, sell: 175});
 
-    expect(suimin.equals(suimin2)).to.be.true;
-    expect(suimin.equals(huuin)).to.be.false;
-  });
+  t.true(suimin.equals(suimin2));
+  t.false(suimin.equals(huuin));
 });
