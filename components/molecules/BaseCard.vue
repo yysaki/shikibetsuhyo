24253d6d47cf1506739c5515441b05235e343396
yysaki <yysaki@mail.yysaki.com>
2018-10-23 01:49:14 +0900

    Add BaseCard.vue

---
+++ components/molecules/BaseCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+    <v-card-title class="primary">
+      <slot name="title"/>
+    </v-card-title>
+    <v-card-text>
+      <slot/>
+    </v-card-text>
+
+    <v-divider/>
+    <v-card-actions>
+      <slot name="actions"/>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  name: "BaseCard"
+}
+</script>
