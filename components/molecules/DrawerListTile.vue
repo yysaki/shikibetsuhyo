b4ae87a00b0052743a392ecf04ce13065d8e8151
yysaki <yysaki@mail.yysaki.com>
2018-10-23 00:59:40 +0900

    Add DrawerListTile

---
+++ components/molecules/DrawerListTile.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-list-tile>
+    <v-list-tile-action>
+      <base-icon :name="icon"/>
+    </v-list-tile-action>
+    <v-list-tile-content>
+      <slot/>
+    </v-list-tile-content>
+  </v-list-tile>
+</template>
+<script>
+import BaseIcon from "~/components/atoms/BaseIcon.vue"
+export default {
+  name: "DrawerListTile",
+  components: { BaseIcon },
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
