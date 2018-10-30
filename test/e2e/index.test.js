b01e5a6953d69a9de435fa3fef32f161953dc9ea
yysaki <yysaki@mail.yysaki.com>
2018-10-30 20:30:56 +0900

    Do render testing by e2e

--- test/e2e/index.test.js
+++ test/e2e/index.test.js
@@ -26,6 +26,77 @@ test.before("Init Nuxt.js", async () => {
   nuxt.listen(4000, "localhost")
 })
 
+test("Route / exits and render HTML", async t => {
+  const page = await browser.newPage()
+  await page.goto("http://localhost:4000")
+  const title = await page.title()
+  t.is(title, "不思議のダンジョン 値段識別表")
+})
+
+test("Route /shiren/ exits and render HTML", async t => {
+  const page = await browser.newPage()
+  await page.goto("http://localhost:4000/shiren/")
+  const title = await page.title()
+  t.is(
+    title,
+    "不思議のダンジョン2 風来のシレン - 不思議のダンジョン 値段識別表"
+  )
+})
+
+test("Route /shiren2/ exits and render HTML", async t => {
+  const page = await browser.newPage()
+  await page.goto("http://localhost:4000/shiren2/")
+  const title = await page.title()
+  t.is(
+    title,
+    "風来のシレン2 ～鬼襲来!シレン城!～ - 不思議のダンジョン 値段識別表"
+  )
+})
+
+test("Route /shiren3/ exits and render HTML", async t => {
+  const page = await browser.newPage()
+  await page.goto("http://localhost:4000/shiren3/")
+  const title = await page.title()
+  t.is(
+    title,
+    "風来のシレン3 からくり屋敷の眠り姫 - 不思議のダンジョン 値段識別表"
+  )
+})
+
+test("Route /shiren4/ exits and render HTML", async t => {
+  const page = await browser.newPage()
+  await page.goto("http://localhost:4000/shiren4/")
+  const title = await page.title()
+  t.is(
+    title,
+    "風来のシレン4 神の眼と悪魔のヘソ - 不思議のダンジョン 値段識別表"
+  )
+})
+
+test("Route /shiren5/ exits and render HTML", async t => {
+  const page = await browser.newPage()
+  await page.goto("http://localhost:4000/shiren5/")
+  const title = await page.title()
+  t.is(
+    title,
+    "風来のシレン5 フォーチュンタワーと運命のダイス - 不思議のダンジョン 値段識別表"
+  )
+})
+
+test("Route /shiren/ exits and render tab header", async t => {
+  const page = await browser.newPage()
+  await page.goto("http://localhost:4000/shiren5/")
+  const tabs = await page.$$eval(".v-tabs__item", ts =>
+    ts.map(tab => tab.textContent.trim())
+  )
+
+  t.true(tabs.includes("腕輪"))
+  t.true(tabs.includes("草"))
+  t.true(tabs.includes("巻物"))
+  t.true(tabs.includes("壺"))
+  t.true(tabs.includes("杖"))
+})
+
 test("Test router-link", async t => {
   const page = await browser.newPage()
   await page.goto("http://localhost:4000")
