0fea0f0ca9db0eca7142c1fb68d82e6db322c8db
yysaki <yysaki@mail.yysaki.com>
2018-11-03 12:58:11 +0900

    Output /sitemap.xml

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,18 @@
+import games from "./plugins/model/game-list.js"
+
 module.exports = {
   buildDir: "functions/nuxt",
-  modules: ["@nuxtjs/google-analytics", "@nuxtjs/vuetify"],
+  modules: ["@nuxtjs/google-analytics", "@nuxtjs/sitemap", "@nuxtjs/vuetify"],
   "google-analytics": {
     id: "UA-48038483-2"
+  },
+  sitemap: {
+    path: "/sitemap.xml",
+    hostname: "https://shikibetsuhyo.yysaki.com",
+    cacheTime: 1000 * 60 * 15,
+    gzip: true,
+    generate: false,
+    routes: callback => callback(null, games.map(g => `/${g.id}/`))
   },
   vuetify: {
     theme: {
