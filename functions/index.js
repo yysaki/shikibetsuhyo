b160e9eabf2f5b2431f705e3ed8e8634fe3a9d8a
yysaki <yysaki@mail.yysaki.com>
2018-11-09 21:47:29 +0900

    Use sitemap.js

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,28 @@
 const functions = require("firebase-functions")
 const { Nuxt } = require("nuxt")
 const express = require("express")
+const sm = require("sitemap")
 const app = express()
+
+const urls = ["shiren", "shiren2", "shiren3", "shiren4", "shiren5"]
+  .map(name => `/${name}/`)
+  .concat(["/"])
+const sitemap = sm.createSitemap({
+  hostname: "https://shikibetsuhyo.yysaki.com",
+  cacheTime: 1000 * 60 * 15,
+  urls: urls.map(url => ({ url: url }))
+})
+
+app.get("/sitemap.xml", (req, res) => {
+  sitemap.toXML((err, xml) => {
+    if (err) {
+      return res.status(500).end()
+    }
+    res.header("Content-Type", "application/xml")
+    res.send(xml)
+    return true
+  })
+})
 
 const nuxt = new Nuxt({ buildDir: "nuxt", dev: false })
 
