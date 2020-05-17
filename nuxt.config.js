c171a8496810ac6488ed7ffe8fc0bd957493fe78
yysaki <mail@yysaki.com>
2020-05-17 14:46:19 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,28 @@
 module.exports = {
   buildDir: "functions/nuxt",
-  modules: ["@nuxtjs/google-analytics", "@nuxtjs/vuetify"],
-  "google-analytics": {
-    id: "UA-48038483-2",
-    dev: false,
-  },
-  vuetify: {
-    icons: {
-      iconfont: "mdi",
-    },
-    theme: {
-      light: true,
-      themes: {
-        light: {
-          primary: "#3e62ad",
-          secondary: "#7986CB",
-          accent: "#9c27b0",
-          error: "#f44336",
-          warning: "#ffeb3b",
-          info: "#2196f3",
-          success: "#4caf50",
+  modules: [
+    ["@nuxtjs/google-analytics", { id: "UA-48038483-2", dev: false }],
+    [
+      "@nuxtjs/vuetify",
+      {
+        icons: {
+          iconfont: "mdi",
+        },
+        theme: {
+          light: true,
+          themes: {
+            light: {
+              primary: "#3e62ad",
+              secondary: "#7986CB",
+              accent: "#9c27b0",
+              error: "#f44336",
+              warning: "#ffeb3b",
+              info: "#2196f3",
+              success: "#4caf50",
+            },
+          },
         },
       },
-    },
-  },
+    ],
+  ],
 }
