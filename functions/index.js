867103de0170efd4ed0c5861a1aaf6107f9ecdef
yysaki <mail@yysaki.com>
2020-05-17 16:00:55 +0900

    functions/index.js: Refer nuxt.config.js

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions")
 const { Nuxt } = require("nuxt")
 const express = require("express")
 const { SitemapStream, streamToPromise } = require("sitemap")
+const config = require("./nuxt.config.js")
 const app = express()
 
 let sitemap
@@ -39,7 +40,12 @@ app.get("/sitemap.xml", (req, res) => {
   }
 })
 
-const nuxt = new Nuxt({ buildDir: "nuxt", dev: false, debug: true })
+const nuxt = new Nuxt({
+  ...config,
+  buildDir: "nuxt",
+  dev: false,
+  debug: true,
+})
 app.use(async (req, res) => {
   await nuxt.ready()
   nuxt.render(req, res)
